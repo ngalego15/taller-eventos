01d11177ed7680658d97312aab1d1bba5ca1ab7f
ngalego15 <nahuelgalego1@gmail.com>
2023-08-08 11:30:17 -0300

    fist commit

---
+++ eventos.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var myDiv = document.getElementById('myDiv');
+
+    myDiv.addEventListener('click', function() {
+        alert('Hola! Soy el div!');
+    });
+});
